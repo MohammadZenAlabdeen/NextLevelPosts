a21e69dd6e2e80565f8ec592e2a215b96b7d5861
MohammadZenAlabdeen <myn1ytc@gmail.com>
2025-03-17 00:35:44 +0300

    added optimization

--- components/form-submit.js
+++ components/form-submit.js
@@ -1,18 +1,18 @@
 "use client";
 
-import { useFormStatus } from "react-dom";
+import { useFormStatus } from 'react-dom';
 
 export default function FormSubmit() {
   const status = useFormStatus();
+
   if (status.pending) {
-    return <p>creating post...</p>;
+    return <p>Creating post...</p>;
   }
+
   return (
     <>
-      <button type="reset" >
-        Reset
-      </button>
-      <button> Create Post</button>
+      <button type="reset">Reset</button>
+      <button>Create Post</button>
     </>
   );
 }
